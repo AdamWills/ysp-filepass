2614ed0dee5de4ff230b9def7891bdf0737ab5f4
Kyle Whittaker <kylewhittaker@me.com>
2023-10-18 11:38:06 -0300

    Added Admin page, page selection, and logic for when to send information.

--- ysp-filepass.php
+++ ysp-filepass.php
@@ -15,177 +15,171 @@ namespace YSP\Filepass;
 
 defined('ABSPATH') || exit;
 
-$test = '';
-$other = "Here is the $test";
+class Revisions {
 
-add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('filepass-styles', plugins_url('/assets/css/style.css', __FILE__));
-});
+    public $webhookURL;
+    public $revisionsPage;
+    public $plugin;
 
-add_action('admin_notices', 'ysp_enterMakeURL');
-function ysp_enterMakeURL()
-{
-    if (get_option('make_webhook_url') === false) {
-        add_settings_error('ysp_filepass', 'missing_url', 'Please add your webhook URL in the Tools » Filepass page.', 'error');
+    public function __construct() {
+        $this->webhookURL = get_option('make_webhook_url');
+        $this->revisionsPage = get_option('filepass_page_id');
+        $this->plugin = plugin_basename(__FILE__);
+    }
+
+    public function register() {
+        add_action('wp_enqueue_scripts', [$this, 'registerStyles']);
+
+        // Make sure webhook and page values are present.
+        add_action('admin_notices', [$this, 'checkRequiredInfo']);
+
+        // Add Settings link to plugin action link.
+        add_filter("plugin_action_links_{$this->plugin}", [$this, 'addSettingsLink']);
+
+        add_action('admin_menu', [$this, 'addAdminPage']);
+        add_action('admin_init', [$this, 'registerRevisionSettings']);
+        add_action('wp', [$this, 'processRevision']);
+    }
+
+    public function registerStyles() {
+        wp_enqueue_style('filepass-styles', plugins_url('/assets/css/style.css', __FILE__));
+    }
+
+    public function checkRequiredInfo() {
+        if (!$this->webhookURL) {
+            add_settings_error('ysp_filepass', 'missing_url', 'Please add your webhook URL in the <a href="tools.php?page=filepass-revisions-options">Tools » Filepass page</a>.', 'error');
+        }
+        if (!$this->revisionsPage) {
+            add_settings_error('ysp_filepass', 'missing_page_id', 'Please select the page you\'d like to use as the Filepass page in the <a href="tools.php?page=filepass-revisions-options">Tools » Filepass page</a>.', 'error');
+        }
+
+        $currentAdmin = get_current_screen()->parent_base;
+        if ($currentAdmin && $this->strContains('options', $currentAdmin)) {
+            return;
+        }
+
         settings_errors('ysp_filepass');
     }
-}
 
-add_action('init', __NAMESPACE__ . '\ysp_processFilepassRequest');
-function ysp_processFilepassRequest()
-{
-    if (get_option('make_webhook_url') === false)
-        return;
-
-    $url = get_option('make_webhook_url');
-
-    if (function_exists('str_contains')) {
-        if (!str_contains($_SERVER['HTTP_REFERER'], 'filepass') || !isset($_SERVER['HTTP_REFERER']))
-            return;
-    } else {
-        if (strpos($_SERVER['HTTP_REFERER'], 'filepass') === false)
-            return;
+    public function addSettingsLink($links) {
+        $settings = '<a href="tools.php?page=filepass-revisions-options">Settings</a>';
+        array_push($links, $settings);
+        return $links;
     }
 
-    if (!isset($_GET['email']) || !isset($_GET['song']) || !isset($_GET['project']) || !isset($_GET['filepass']))
-        return;
-
-    $email = sanitize_email($_GET['email']);
-    $song = sanitize_text_field($_GET['song']);
-    $project = sanitize_text_field($_GET['project']);
-    $filepass = sanitize_text_field($_GET['filepass']);
-
-    $data = array(
-        'email' => $email,
-        'song' => $song,
-        'project' => $project,
-        'filepass' => $filepass,
-    );
-
-    if (!$email || !$song || !$project || !$filepass) {
-        $newError = 'Filepass » Unexpected Values: ' . print_r($data, 1);
-        error_log($newError);
+    public function addAdminPage() {
+        add_submenu_page(
+            // Parent menu slug (Tools)
+            'tools.php',
+            // Page title
+            'Filepass Revisions Options',
+            // Menu title
+            'Filepass Tool',
+            // Capability
+            'manage_options',
+            // Menu slug
+            'filepass-revisions-options',
+            [$this, 'renderOptionsPage'] // Callback function to render the page
+        );
     }
 
-    // ysp_sendFilepassRequest($url, $data);
-}
+    public function renderOptionsPage() {
+        $filepass_url = get_option('make_webhook_url', ''); // Retrieve the current value
+        $filepass_page = get_option('filepass_page_id', ''); // Retrieve the current value
+        $pageArgs = [
+            'name' => 'filepass_page_id',
+            'id' => 'filepass_page_id',
+            'class' => 'regular-text',
+            'echo' => 0,
+            'selected' => $filepass_page
+        ];
+        $pages = wp_dropdown_pages($pageArgs);
 
-function ysp_sendFilepassRequest($url, $data)
-{
-    $response = wp_remote_post($url, [
-        'headers' => [
-            'Content-Type' => 'application/json'
-        ],
-        'body' => wp_json_encode($data)
-    ]);
+?>
+        <div class="wrap">
+            <h1>Filepass Tool Options</h1>
+            <form method="post" action="options.php">
+                <?php settings_fields('make_webhook_url_options_group'); ?>
+                <table class="form-table" role="presentation">
+                    <tbody>
+                        <tr>
+                            <th>Webhook URL</th>
+                            <td>
+                                <input class="regular-text" type="text" id="make_webhook_url" name="make_webhook_url" value="<?php echo esc_attr($filepass_url); ?>" />
+                                <p><small>Enter the Webhook URL.</small></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Revisions Page</th>
+                            <td>
+                                <?php echo $pages; ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <?php submit_button(); ?>
+            </form>
+        </div>
+<?php
+    }
 
-    if (is_wp_error($response)) {
-        error_log('Error: ' . $response->get_error_message());
+    public function registerRevisionSettings() {
+        register_setting('make_webhook_url_options_group', 'make_webhook_url', 'esc_url_raw');
+        register_setting('make_webhook_url_options_group', 'filepass_page_id', 'integer');
+    }
+
+    public function processRevision() {
+        if (!$this->webhookURL || !$this->revisionsPage || is_page($this->revisionsPage) === false) return;
+
+        if (!$this->strContains('filepass', $_SERVER['HTTP_REFERER']) || !isset($_SERVER['HTTP_REFERER'])) return;
+
+        if (!isset($_GET['email']) || !isset($_GET['song']) || !isset($_GET['project']) || !isset($_GET['filepass'])) return;
+
+        $email = sanitize_email($_GET['email']);
+        $song = sanitize_text_field($_GET['song']);
+        $project = sanitize_text_field($_GET['project']);
+        $filepass = sanitize_text_field($_GET['filepass']);
+
+        $data = array(
+            'email' => $email,
+            'song' => $song,
+            'project' => $project,
+            'filepass' => $filepass,
+        );
+
+        if (!$email || !$song || !$project || !$filepass) {
+            $newError = 'Filepass » Unexpected Values: ' . print_r($data, 1);
+            error_log($newError);
+        }
+
+        $this->sendRevisionRequest($this->webhookURL, $data);
+    }
+
+    public function sendRevisionRequest($url, $data) {
+        $response = wp_remote_post($url, [
+            'headers' => [
+                'Content-Type' => 'application/json'
+            ],
+            'body' => wp_json_encode($data)
+        ]);
+
+        if (is_wp_error($response)) {
+            error_log('Error: ' . $response->get_error_message());
+        }
+    }
+
+    private function strContains($needle, $haystack) {
+        if (function_exists('str_contains')) {
+            return str_contains($haystack, $needle);
+        } else {
+            if (stripos($haystack, $needle) === false) {
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
 }
 
-// Add a submenu page under Tools
-add_action('admin_menu', 'ysp_addFileOptionsPage');
-function ysp_addFileOptionsPage()
-{
-    add_submenu_page(
-        'tools.php',
-        // Parent menu slug (Tools)
-        'Filepass Revisions Options',
-        // Page title
-        'Filepass Tool',
-        // Menu title
-        'manage_options',
-        // Capability
-        'filepass-revisions-options',
-        // Menu slug
-        'ysp_renderFilepassOptionsPage' // Callback function to render the page
-    );
-}
-
-// Render the options page content
-function ysp_renderFilepassOptionsPage()
-{
-    $filepass_url = get_option('make_webhook_url', ''); // Retrieve the current value
-    $filepass_page = get_option('filepass_page_id', ''); // Retrieve the current value
-    $pageArgs = [
-        'name' => 'filepass_page_id',
-        'id' => 'filepass_page_id',
-        'echo' => 0,
-        'selected' => $filepass_page
-    ];
-    $pages = wp_dropdown_pages($pageArgs);
-
-    echo '<div class="wrap">';
-    echo '<h1>Filepass Tool Options</h1>';
-    echo '<form method="post" action="options.php">';
-    settings_fields('make_webhook_url_options_group');
-    echo '<label for="make_webhook_url">Filepass URL:</label>';
-    echo '<input type="text" id="make_webhook_url" name="make_webhook_url" value="' . esc_attr($filepass_url) . '" />';
-    echo '<p><small>Enter the Webhook URL.</small></p>';
-    echo '<label for="filepass_page_id">Select the page for Filepass</label>: ';
-    echo $pages;
-    submit_button();
-    echo '</form>';
-    echo '</div>';
-}
-
-// Register the settings and sanitize the input
-add_action('admin_init', 'ysp_registerFilepassSettings');
-function ysp_registerFilepassSettings()
-{
-    register_setting('make_webhook_url_options_group', 'make_webhook_url', 'esc_url_raw');
-    register_setting('make_webhook_url_options_group', 'filepass_page_id', 'integer');
-}
-
-
-add_shortcode('FILEPASS_REVISIONS', 'ysp_filepassRevisions');
-function ysp_filepassRevisions($atts, $content = null)
-{
-
-    // Disable during development
-    // This double checks that the traffic is coming from Filepass to prevent false triggers
-    // if (strpos($_SERVER['HTTP_REFERER'], 'filepass') === false) return 'Yikes! You\'re not from Filepass!';
-
-    $data = shortcode_atts([
-        'make_url' => null
-    ], $atts);
-
-    if ($data['make_url'] === null)
-        return 'Missing value for "make_url"';
-    if (!isset($_GET['email']) || !isset($_GET['song']) || !isset($_GET['project']))
-        return 'Missing email, song, or project data.';
-    if (!is_email($_GET['email']))
-        return 'Whoops! It looks like the email provided is not a valid email address.';
-
-    if (isset($_GET['filepass'])) {
-        $filepassArray = explode('/', $_GET['filepass']);
-        $filepassURL = str_replace(end($filepassArray), urlencode(end($filepassArray)), $_GET['filepass']);
-    } else {
-        $filepassURL = '';
-    }
-    $fileExtension = str_contains($_GET['song'], '.wav') ? '.wav' : '.mp3';
-    $song = explode($fileExtension, $_GET['song'])[0];
-    echo '<p class="ysp_filepass">' . $content . '</p>';
-
-    $url = $data['make_url'];
-    $body = [
-        'email' => $_GET['email'],
-        'song' => $song,
-        'project' => $_GET['project'],
-        'filepass_url' => $filepassURL,
-        'date' => date("m/d/Y H:i:s")
-    ];
-
-    $response = wp_remote_post($url, [
-        'headers' => [
-            'Content-Type' => 'application/json'
-        ],
-        'body' => wp_json_encode($body)
-    ]);
-
-    if (is_wp_error($response)) {
-        return '<h2>Error!</h2><pre>' . print_r($response, 1) . '</pre>';
-    }
-    return;
-}
+$revisions = new Revisions();
+$revisions->register();
